33a82728b8fc616c799d16d48325ef82fdcaba03
RationalPrabal <prabal19999@gmail.com>
2022-12-14 16:54:17 +0530

    init

---
+++ good-cluck/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WithSubnavigation from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <WithSubnavigation/>
+    </div>
+  );
+}
+
+export default App;
